5bdec5bbdfc6200448e214dcbfb30901ee36f359
Valdymarych <student21_8a22@lpml.com.ua>
2024-05-11 21:43:12 +0300

    mainpage scaleable

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -10,22 +10,6 @@ let MainPage = (props) => {
             <header className={styles.header}>
                 <img src={famico_text} alt="famico" className={styles.header_image}></img>
             </header>
-
-            <div className={styles.about}>
-                <div className={styles.about_header}>
-                    Про нас 
-                </div>
-                <div className={styles.triangle}>
-                    <img className={styles.triangle_img} src={green_triangle} alt="*"></img>
-                </div>
-                <div className={styles.about_text}>
-                    <div className={styles.welcome}>
-                        <h4>Вас вітає Фаміко!</h4>
-                    </div>
-                    Текст vb rjvfylf pijpierm g'oinermg'nire'ner'oin'eornco'e rnvoernvojrnveojngr jernbglrjngelrkngtelrkn telrnf verlfn
-                </div>
-                <div className={styles.border_handler}></div>
-            </div>
             <div className={styles.contest}>
                 <div className={styles.contest_header}>
                     Lucky Shapes
@@ -51,6 +35,22 @@ let MainPage = (props) => {
                     <img src={poster} alt="poster" className={styles.contest_poster_img}></img>
                 </div>             
             </div>
+            <div className={styles.about}>
+                <div className={styles.about_header}>
+                    Про нас 
+                </div>
+                <div className={styles.triangle}>
+                    <img className={styles.triangle_img} src={green_triangle} alt="*"></img>
+                </div>
+                <div className={styles.about_text}>
+                    <div className={styles.welcome}>
+                        <h4>Вас вітає Фаміко!</h4>
+                    </div>
+                    Текст vb rjvfylf pijpierm g'oinermg'nire'ner'oin'eornco'e rnvoernvojrnveojngr jernbglrjngelrkngtelrkn telrnf verlfn
+                </div>
+                <div className={styles.border_handler}></div>
+            </div>
+
 
 
         </div>
